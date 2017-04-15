ffe474f7ea7fea96fba6a67e2e145c95c91641f9
christopher bowen <christopherbowen@mail.weber.edu>
2017-04-15 01:44:22 +0000

    2nd commit

--- teamAwesome_hw11_task1.c
+++ teamAwesome_hw11_task1.c
@@ -2,6 +2,8 @@
  * =====================================================================================
  *
  *       Filename:  housePrice.c
+ *
+ *
  *       	Usage:  ./housePrice.c
  *    Description:  Ask user to specify the address and price
  *    				of two house that are on sale
@@ -11,7 +13,7 @@
  *        Created:  03/06/2014 04:03:12 PM
  *       Compiler:  gcc
  *         Author:  Dr. Hugo Valle (), hugovalle1@weber.edu
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
@@ -19,17 +21,47 @@
 #include <stdlib.h>
 #include "teamAwesome_hw11_task1.h" // Rename this to your name name_hw#_task#.h
 /* Function Prototypes */
-struct house InputHouse(void)
+struct House InputHouse(void)
 {
+    int count = 0;
+    char c;
+    struct House home;
     printf("Enter in a street number: ");
-    scanf("%d", &House
+    scanf("%d", &home.num);
+    printf("\nEnter a street name: ");
+    while((c = getchar())!= '\n')
+    {
+        home.name[count]=c;
+        count++;
+    }
+    printf("Enter the price: ");
+    scanf("%f", &home.price);
+    return home;
 }
-void PrintHouse(char *name, struct house h)
+void PrintHouse(char *name, struct House h)
 {
-    //
+    printf("%s: at %d %s for [%f]\n", name, h.num, h.name, h.price);
+    return;
 }
-int ComparePrices(struct house first, struct house second)
+int ComparePrices(struct House first, struct House second)
 {
-    //
+    float home1 = first.price;
+    float home2 = second.price;
+    int cmp;
+    if(home1 < home2)
+    {
+        cmp = -1;
+    }
+    else if(home1 > home2)
+    {
+        cmp = 1;
+    }
+    else
+    {
+        cmp=0;
+    }
+    return cmp;
 }
+
+
 
