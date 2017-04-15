d86371c24d7fdbd17e1d1a1d565309c9144e5ea3
christopher bowen <christopherbowen@mail.weber.edu>
2017-04-15 00:16:04 +0000

    initial commit

---
+++ teamAwesome_hw11_task1.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  housePrice.c
+ *       	Usage:  ./housePrice.c
+ *    Description:  Ask user to specify the address and price
+ *    				of two house that are on sale
+ *    				It prints out which of the two houses is cheaper
+ *
+ *        Version:  1.0
+ *        Created:  03/06/2014 04:03:12 PM
+ *       Compiler:  gcc
+ *         Author:  Dr. Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "teamAwesome_hw11_task1.h" // Rename this to your name name_hw#_task#.h
+/* Function Prototypes */
+struct house InputHouse(void)
+{
+    printf("Enter in a street number: ");
+    scanf("%d", &House
+}
+void PrintHouse(char *name, struct house h)
+{
+    //
+}
+int ComparePrices(struct house first, struct house second)
+{
+    //
+}
+
